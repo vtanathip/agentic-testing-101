b35dc6f6a5a8b906d9db0e30cd3ff981d7ed7e68
LABTOP-VTANATHIP <tanathip.se@gmail.com>
2025-08-28 23:25:53 +0700

    initial agentic testing with local MCP server

--- README.md
+++
@@ -1,2 +0,0 @@
-# agentic-testing-101
-A sample approach of using LangChain and Playwright MCP to run a test
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from agentic-testing-101!")
+
+
+if __name__ == "__main__":
+    main()
